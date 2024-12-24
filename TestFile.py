75f9b9f2668d352a45b35a8fec9a2191aaafc8fa
ScifiWarlock <rakmot19@gmail.com>
2024-12-24 14:15:41 -0800

    got images and functions working!

--- TestFile.py
+++ TestFile.py
@@ -5,8 +5,12 @@ ld.clear()
 ld.preamble("article", "testing with python", "Omkar Tasgaonkar")
 ld.ong_bruh()
 
-ld.line("Hello whats up, welcome to my first try", True)
+ld.line("Hello whats up, ;math('welcome');", True)
 ld.line("This is after a double space, now a single space", False)
+ld.image(14.06)
+ld.line("", False)
+ld.line("As you can see we have inserted a screeenshot above for the eigenvector ;math('v');", True)
+
 ld.gahzamn()
 
 ld.compile()
