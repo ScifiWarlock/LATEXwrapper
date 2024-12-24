684df32759004ba6510aeb5c9a4115fea08df426
ScifiWarlock <rakmot19@gmail.com>
2024-12-24 15:33:57 -0800

    greek letters and integrals!

--- LATEXdefintions.py
+++ LATEXdefintions.py
@@ -5,7 +5,27 @@ import datetime
 date = datetime.date.today()
 ss_date = date.strftime("%Y-%m-%d")
 
+greek_dic = {'alpha':fr'\alpha', 
+             'beta':fr'\beta', 
+             'gamma':fr'\gamma',
+             'delta':fr'\delta',
+             'epsilon':fr'\varepsilon',
+             'zeta':fr'\zeta',
+             'eta':fr'\eta',
+             'theta':fr'\theta',
+             'lambda':fr'\lamda',
+             'mu':fr'\mu',
+             'pi':fr'\pi'}
+
 ss_dir = "/Users/omkar/Desktop/Screenshots/"
+
+#get a greek letter from the dictionary
+def greek(name):
+    return greek_dic.get(name)
+
+def give_infinity():
+    infinity = "\infty"
+    return infinity
 
 def create(name):
     global fl
@@ -29,6 +49,15 @@ def preamble(type, title, author):
     fl.write(fr"\graphicspath" + "{" + texcurl(fr"{ss_dir}") + "}" + "\n")
     fl.write("\n")
 
+#add a list of figures in the notes and then do a pagebreak for formatting purposes
+def figure_index():
+    fl.write(fr"\listoffigures" + "\n")
+    fl.write(fr"\pagebreak" + "\n")
+
+#adds a list of topics and sections/subsections within the doc
+def topic_index():
+    fl.write(fr"\tableofcontents" + "\n")
+
 #ong_bruh lets begin
 def ong_bruh():
     fl.write(fr"\begin{{document}}" + "\n")
@@ -42,7 +71,7 @@ def line(string, double):
     if ";" in string:
         split_list = string.split(";")
         for element in split_list:
-            if "(" in element:
+            if "'" in element:
                 retstring += str(eval(element))
             else:
                 retstring += element
@@ -62,15 +91,38 @@ def math(string):
     return "$" + string + "$"
 
 #time must be in hh.mm and 12 hour time not 24
-def image(time):
+def image(time, capt):
     for filename in os.listdir(ss_dir):
         if str(time) in filename:
             fl.write("\n")
 
             #format it so that it fits the text flow
             fl.write(fr"\begin{{figure}}[h]" + "\n")
+            fl.write(fr"\caption" + texcurl(fr"{capt}") + "\n")
+            fl.write(fr"\centering" + "\n")
             fl.write(fr"\includegraphics" + "[width=5cm, height=5cm]" + texcurl(fr"{filename}") + "\n")
-            fl.write(fr"\end{{figure}}")
+            fl.write(fr"\end{{figure}}" + "\n")
+
+#use this for new topic or slide
+def topic(string):
+    fl.write("\n")
+    fl.write(fr"\subsection" + texcurl(fr"{string}") + "\n")
+
+#use this for an example problem
+def example(string):
+    fl.write("\n")
+    fl.write(fr"\subsubsection" + texcurl(fr"{string}") + "\n")
+
+#definite and indefinite single integrals
+def integral(func, var, lower = None, upper = None):
+    if lower != None and upper != None:
+        return (fr"\(\int_" + texcurl(fr"{lower}") + "^" + texcurl(fr"{upper}") + fr" {func} \," + fr"d{var} \)")
+    else:
+        return (fr"\(\int" + fr" {func} \," + fr"d{var} \)")  
+
+#sqrt function    
+def sqrt(string):
+    return fr"\sqrt{string}"
 
 #end the script
 def gahzamn():
